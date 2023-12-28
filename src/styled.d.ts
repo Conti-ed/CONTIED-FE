03614b20c2745dcdc98a3bc7a8286f1f91772394
Jooyul Yoon <juyeolyoon@gmail.com>
2023-12-28 10:17:13 +0900

    initialize project

---
+++ src/styled.d.ts
@@ -0,0 +1,17 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    bgColor: string;
+    titleColor: string;
+    textColor: string;
+    accentColor: string;
+    tabBgColor: string;
+    cardColor: string;
+    greenColor: string;
+    redColor: string;
+    blackColor: string;
+    toggleBorder: string;
+    gradient: string;
+  }
+}
