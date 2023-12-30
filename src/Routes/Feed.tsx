cf827091363e78aac92078e6a5615c788e63a3a2
Jooyul Yoon <juyeolyoon@gmail.com>
2023-12-30 15:12:01 +0900

    feat: add feed template

--- src/Routes/Feed.tsx
+++ src/Routes/Feed.tsx
@@ -1,5 +1,21 @@
+import { styled } from "styled-components";
+import Conti from "../components/Conti";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 10px;
+`;
+
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <Container>
+      {Array.from({ length: 100 }).map((_, index) => (
+        <Conti key={index}></Conti>
+      ))}
+    </Container>
+  );
 }
 
 export default Feed;
