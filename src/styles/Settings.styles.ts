ddce56c652f2240b6e67a497372bc89d5fd98e0b
노준석 <joshua052698@gmail.com>
2024-01-05 18:09:06 +0900

    Remove: DarkModeSwitch component

--- src/styles/Settings.styles.ts
+++ src/styles/Settings.styles.ts
@@ -86,55 +86,6 @@ export const ToggleSwitch = styled.label`
   }
 `;
 
-export const DarkModeSwitch = styled.label`
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-
-  & input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
-
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: 0.4s;
-    border-radius: 34px;
-  }
-
-  .slider:before {
-    position: absolute;
-    content: '';
-    height: 26px;
-    width: 26px;
-    left: 4px;
-    bottom: 4px;
-    background-color: white;
-    transition: 0.4s;
-    border-radius: 50%;
-  }
-
-  input:checked + .slider {
-    background-color: #2b4485;
-  }
-
-  input:focus + .slider {
-    box-shadow: 0 0 1px #2b4485;
-  }
-
-  input:checked + .slider:before {
-    transform: translateX(26px);
-  }
-`;
-
 export const ToggleContainer = styled.div<{
   $isdarkmode: boolean;
   $moonbackimg: string;
@@ -170,13 +121,4 @@ export const ToggleCircle = styled.span<{
   background-image: ${({ $isdarkmode, $moonimg, $sunimg }) =>
     $isdarkmode ? `url(${$moonimg})` : `url(${$sunimg})`};
   background-size: cover;
-`;
-
-export const BottomNav = styled.nav`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  display: flex;
-  justify-content: space-around;
-  padding: 10px 0;
 `;
