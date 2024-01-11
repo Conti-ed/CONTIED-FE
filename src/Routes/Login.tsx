83767cbb6ca8068653d159464ac62780505ceebf
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-11 12:57:00 +0900

    fix: kakao login request url

--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -1,6 +1,7 @@
-import KakaoLogin from 'react-kakao-login';
-import { useNavigate } from 'react-router-dom';
-import { Container } from '../styles/Login.styles';
+import KakaoLogin from "react-kakao-login";
+import { useNavigate } from "react-router-dom";
+import { Container } from "../styles/Login.styles";
+import { SERVER_URL } from "../api";
 
 function Login() {
   const navigate = useNavigate();
@@ -10,23 +11,23 @@ function Login() {
     console.log(response.response.access_token);
 
     let res = await fetch(
-      `http://127.0.0.1:8000/rest-auth/kakao/?code=${response.response.access_token}`,
+      `${SERVER_URL}/rest-auth/kakao/?code=${response.response.access_token}`,
       {
-        method: 'GET',
+        method: "GET",
       }
     );
     let data = await res.json();
-    localStorage.setItem('accessToken', data.kakao_data.access_token);
-    localStorage.setItem('refreshToken', data.kakao_data.refresh_token);
-    localStorage.setItem('user_info', JSON.stringify(data.user_info));
+    localStorage.setItem("accessToken", data.kakao_data.access_token);
+    localStorage.setItem("refreshToken", data.kakao_data.refresh_token);
+    localStorage.setItem("user_info", JSON.stringify(data.user_info));
     console.log(res, data);
 
-    navigate('/');
+    navigate("/");
   };
   return (
     <Container>
       <KakaoLogin
-        token={'ca220974886e2ef4eb4a37d21b258d7c'}
+        token={"ca220974886e2ef4eb4a37d21b258d7c"}
         onSuccess={kakaoResponse}
         onFail={console.error}
         onLogout={console.info}
