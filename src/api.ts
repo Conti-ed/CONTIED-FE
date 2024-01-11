5921334e1002018845401869e96985c7c9eb55bd
Jooyul Yoon <juyeolyoon@gmail.com>
2024-01-11 12:45:42 +0900

    goorm server url

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,7 @@
-export const SERVER_URL = "http://127.0.0.1:8000";
+export const SERVER_URL =
+  "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
+// export const SERVER_URL =
+// "http://127.0.0.1:8000";
 
 export async function getKeywords() {
   return (await fetch(`${SERVER_URL}/api/keyword`)).json();
