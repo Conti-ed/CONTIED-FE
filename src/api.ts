2dd9fff4a41631fdfd1c3b3ade42c5ef22e57b32
Jooyul Yoon <juyeolyoon@gmail.com>
2024-02-05 21:09:18 -0800

    login

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,53 @@
-export const SERVER_URL =
-  "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
-// export const SERVER_URL = "http://127.0.0.1:8000";
+// export const SERVER_URL =
+//   "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
+export const SERVER_URL = "http://127.0.0.1:8000";
+const access_token = localStorage["accessToken"];
 
 export async function getKeywords() {
-  return (await fetch(`${SERVER_URL}/api/keyword`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/keyword`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
 
 export async function getConties() {
-  return (await fetch(`${SERVER_URL}/api/conti`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/conti`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
 
 export async function getConti(cid: number) {
-  return (await fetch(`${SERVER_URL}/api/conti/${cid}`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/conti/${cid}`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
 
 export async function getMyConties() {
   const my_id = JSON.parse(localStorage["user_info"]).id;
-  return (await fetch(`${SERVER_URL}/api/conti?user=${my_id}`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/conti?user=${my_id}`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
 
 export async function getContiesByKeyword(keyword: string) {
-  return (await fetch(`${SERVER_URL}/api/conti?keyword=${keyword}`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/conti?keyword=${keyword}`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
 
 export async function getSongsByKeyword(keyword: string) {
-  return (await fetch(`${SERVER_URL}/api/song?keyword=${keyword}`)).json();
+  return (
+    await fetch(`${SERVER_URL}/api/song?keyword=${keyword}`, {
+      headers: { Authorization: `Bearer ${access_token}` },
+    })
+  ).json();
 }
