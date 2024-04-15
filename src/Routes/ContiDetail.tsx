9c1c1da265ecb7e725d99052113acb449a3f0571
Jooyul Yoon <juyeolyoon@gmail.com>
2024-04-15 13:32:51 -0700

    fix: undefined uid

--- src/Routes/ContiDetail.tsx
+++ src/Routes/ContiDetail.tsx
@@ -213,7 +213,9 @@ function ContiDetail() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const { id: cid } = useParams();
-  const uid = JSON.parse(localStorage["user_info"]).id;
+  const uid = localStorage["user_info"]
+    ? JSON.parse(localStorage["user_info"]).id
+    : null;
   const { state, updateState, onDragEnd, toggleFavorite, songOptionsClick } =
     useContiDetailState(Number(cid), uid);
   const [modal, setModal] = useRecoilState(modalAtom);
@@ -226,9 +228,7 @@ function ContiDetail() {
       updateState({
         contiData: data,
         songs: data ? data.songs.sort((a, b) => a.order - b.order) : [],
-        isOwner: data
-          ? data.owner.id === JSON.parse(localStorage["user_info"]).id
-          : false,
+        isOwner: data ? data.owner.id === uid : false,
         editTitleValue: data!.title,
       });
     },
@@ -239,13 +239,11 @@ function ContiDetail() {
     queryKey: ["saved"],
     queryFn: () => getSavedConties(uid),
     onSuccess: (data) => {
-      if (data.find((c) => c === Number(cid))) {
+      if (data && data.find((c) => c === Number(cid))) {
         updateState({ isFavorite: true });
       }
     },
   });
-  console.log(savedConties);
-  console.log(state.isFavorite);
 
   // Refetch conti data
   useEffect(() => {
@@ -367,15 +365,17 @@ function ContiDetail() {
                 {state.isOwner ? (
                   <BorderColorIcon />
                 ) : (
-                  <HeartIcon
-                    $isFavorite={state.isFavorite}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    stroke="lightcoral"
-                  >
-                    <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"></path>
-                  </HeartIcon>
+                  uid && (
+                    <HeartIcon
+                      $isFavorite={state.isFavorite}
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      stroke="lightcoral"
+                    >
+                      <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"></path>
+                    </HeartIcon>
+                  )
                 )}
               </EditIconContainer>
               {state.showOwnerMenu && (
