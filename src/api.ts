e60ecf65b7ec73aae1a9dcda2560bf05c51eed42
노준석 <joshua052698@gmail.com>
2024-06-08 20:08:25 +0900

    Fix: api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
-export const SERVER_URL =
-  "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
-// export const SERVER_URL = "http://127.0.0.1:8000";
+// export const SERVER_URL =
+//   "https://vscode-adakm.run-us-west2.goorm.site/proxy/8000";
+export const SERVER_URL = "http://127.0.0.1:8000";
 
 export async function getKeywords() {
   return (await fetch(`${SERVER_URL}/api/keyword`)).json();
