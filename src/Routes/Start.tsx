42b5520cb2ad3da1e28b4c9022a83a258ade4d2e
노준석 <joshua052698@gmail.com>
2024-06-25 22:25:21 +0900

    💄 Apply SVG sprite to Start

--- src/Routes/Start.tsx
+++ src/Routes/Start.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled, { keyframes, css } from "styled-components";
+import Icon from "../components/Icon";
 
 const fadeOut = keyframes`
   from {
@@ -76,33 +77,6 @@ const TextContainer = styled.span`
   margin-left: 10px; /* Text와 Icon 사이의 간격 */
 `;
 
-const KakaoIcon = () => (
-  /* 카카오 아이콘 */
-  <svg
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g clipPath="url(#clip0_924_3971)">
-      <path
-        d="M32 29.5C32 30.8808 30.8808 32 29.5 32H2.5C1.11925 32 0 30.8808 0 29.5V2.5C0 1.11925 1.11925 0 2.5 0H29.5C30.8808 0 32 1.11925 32 2.5V29.5Z"
-        fill="#FFE812"
-      />
-      <path
-        d="M15.6361 8.72754C11.017 8.72754 7.27246 11.6488 7.27246 15.2522C7.27246 17.5819 8.83791 19.6261 11.1928 20.7804C11.0646 21.2176 10.3695 23.5927 10.3418 23.7793C10.3418 23.7793 10.3252 23.9195 10.4169 23.973C10.5087 24.0264 10.6166 23.9849 10.6166 23.9849C10.8798 23.9485 13.668 22.0107 14.1506 21.6742C14.6327 21.7418 15.1291 21.7769 15.6361 21.7769C20.2552 21.7769 23.9997 18.8557 23.9997 15.2522C23.9997 11.6488 20.2552 8.72754 15.6361 8.72754Z"
-        fill="black"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_924_3971">
-        <rect width="32" height="32" rx="16" fill="white" />
-      </clipPath>
-    </defs>
-  </svg>
-);
-
 const Start: React.FC = () => {
   const [isFading, setIsFading] = React.useState(false);
   const navigate = useNavigate();
@@ -122,7 +96,7 @@ const Start: React.FC = () => {
       <ButtonContainer>
         <StartButton onClick={handleButtonClick}>
           <IconContainer>
-            <KakaoIcon />
+            <Icon id="kakao-start" width="32" height="32" />
           </IconContainer>
           <TextContainer>카카오로 3초 만에 시작하기</TextContainer>
         </StartButton>
